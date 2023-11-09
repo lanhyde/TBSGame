62b20ae591ea9bab7d4ec6d91e1d099b3ff05272
lan hyde <lanhaide@hotmail.com>
2023-11-10 08:11:52 +0900

    update enemy AI (part 1)

--- Assets/Scripts/AI/EnemyAI.cs
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -7,6 +7,20 @@ public class EnemyAI : MonoBehaviour
 {
     private float timer;
 
+    private enum State
+    {
+        WaitingForEnemyTurn,
+        TakingTurn,
+        Busy
+    }
+
+    private State state;
+
+    private void Awake()
+    {
+        state = State.WaitingForEnemyTurn;
+    }
+
     private void Start()
     {
         TurnSystem.Instance.OnTurnChanged += OnTurnChanged;
@@ -24,16 +38,41 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        timer -= Time.deltaTime;
-        if (timer <= 0f)
+        switch (state)
         {
-            TurnSystem.Instance.NextTurn();
-            
+            case State.WaitingForEnemyTurn:
+                break;
+            case State.TakingTurn:
+                timer -= Time.deltaTime;
+                if (timer <= 0f)
+                {
+                    state = State.Busy;
+                    TakeEnemyAIAction(SetStateTakingTurn);
+                }
+                break;
+            case State.Busy:
+                break;
         }
     }
 
     private void OnTurnChanged(object sender, EventArgs args)
     {
+        if (TurnSystem.Instance.IsPlayerTurn())
+        {
+            return;
+        }
+
+        state = State.TakingTurn;
         timer = 2f;
+    }
+
+    private void SetStateTakingTurn()
+    {
+        timer = 0.5f;
+        state = State.TakingTurn;
+    }
+    private void TakeEnemyAIAction(Action completionCallback) 
+    {
+        
     }
 }
---
+++ Assets/Scripts/Unit/UnitManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour
+{
+    private List<Unit> unitList;
+    private List<Unit> friendUnitList;
+    private List<Unit> enemyUnitList;
+}
