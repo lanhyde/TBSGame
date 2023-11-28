71b982f0f56be49c614a1afaf369ca711dd330c8
lan hyde <lanhaide@hotmail.com>
2023-11-28 22:43:22 +0900

    refactor inputs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -29,16 +29,8 @@ public class CameraController : MonoBehaviour
 
     private void HandleZoom()
     {
-        float zoomAmount = 1f;
-        if (Input.mouseScrollDelta.y > 0)
-        {
-            targetFollowOffset.y -= zoomAmount;
-        }
-
-        if (Input.mouseScrollDelta.y < 0)
-        {
-            targetFollowOffset.y += zoomAmount;
-        }
+        float zoomIncreaseAmount = 1f;
+        targetFollowOffset.y += InputManager.Instance.GetCameraZoomAmount() * zoomIncreaseAmount;
 
         targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
         float zoomSpeed = 5f;
@@ -48,15 +40,7 @@ public class CameraController : MonoBehaviour
     private void HandleRotation()
     {
         Vector3 rotationVector = Vector3.zero;
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotationVector.y = 1f;
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            rotationVector.y = -1f;
-        }
+        rotationVector.y = InputManager.Instance.GetCameraRotateAmount();
 
         float angularSpeed = 100f;
         transform.eulerAngles += rotationVector * angularSpeed * Time.deltaTime;
@@ -64,29 +48,10 @@ public class CameraController : MonoBehaviour
 
     private void HandleTranslation()
     {
-        Vector3 inputMoveDir = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputMoveDir.z += 1f;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputMoveDir.z -= 1f;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputMoveDir.x -= 1f;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputMoveDir.x += 1f;
-        }
+        Vector2 inputMoveDir = InputManager.Instance.GetCameraMoveVector();
 
         float moveSpeed = 10f;
-        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+        Vector3 moveVector = transform.forward * inputMoveDir.y + transform.right * inputMoveDir.x;
         transform.position += moveVector * moveSpeed * Time.deltaTime;
     }
 }
